38038ada45449ef7f8ae27c045d7ce399d55cb5c
Daniel <danmobbs.101@gmail.com>
2021-09-18 15:23:34 +0100

    Start of building plugin

---
+++ dsm-Theme_Page_Finder.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @package dsm Theme Page Finder
+ */
+/*
+Plugin Name: dsm Theme Page Finder
+Plugin URI: #
+Description: This plugin will check to see if your WordPress website has the basic pages, ie Home page, About Page, Blog Page and Contact Page.
+Version: 0.0.2
+Author: Daniel Mobbs
+Author: https://danmobbs.co
+License: GPLv2 or later
+Text Domain: dsm Theme Page Finder
+*/
